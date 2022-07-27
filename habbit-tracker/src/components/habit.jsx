73292676f5acaf2d9b7381379c1659fc5a230e28
rnfkaus8 <kth4350@gmail.com>
2022-07-27 19:14:55 +0900

    modify : Habits Component 에서 count 증가, 감소, 해당 Habit Component 삭제 기능 추가

--- habbit-tracker/src/components/habit.jsx
+++ habbit-tracker/src/components/habit.jsx
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
-
   handleIncrement = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
+    this.props.onIncrement(this.props.habit);
   };
 
   handleDecreament = () => {
-    const count = this.state.count - 1;
-    console.log(count);
-    this.setState({
-      count: count < 0 ? 0 : count,
-    });
+    this.props.onDecreament(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
   };
 
   render() {
@@ -37,7 +31,10 @@ class Habit extends Component {
         >
           <i className="fa-solid fa-square-minus"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i className="fa-solid fa-trash"></i>
         </button>
       </li>
--- habbit-tracker/src/components/habits.jsx
+++ habbit-tracker/src/components/habits.jsx
@@ -9,11 +9,38 @@ class Habits extends Component {
       { id: 3, name: "Coding", count: 0 },
     ],
   };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const idx = habits.indexOf(habit);
+    habits[idx].count++;
+    this.setState({ habits });
+  };
+
+  handleDecreament = (habit) => {
+    const habits = [...this.state.habits];
+    const idx = habits.indexOf(habit);
+    const count = habits[idx].count - 1;
+    habits[idx].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
   render() {
     return (
       <ul>
         {this.state.habits.map((habit) => (
-          <Habit key={habit.id} habit={habit} />
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecreament={this.handleDecreament}
+            onDelete={this.handleDelete}
+          />
         ))}
       </ul>
     );
