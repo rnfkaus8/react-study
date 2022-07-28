54a6d356f1139d1464ee424215063cc22e129eca
rnfkaus8 <kth4350@gmail.com>
2022-07-28 11:11:58 +0900

    modify : App Component 로 habits state 이동, HabitAddForm Component 추가

--- habbit-tracker/src/components/habits.jsx
+++ habbit-tracker/src/components/habits.jsx
@@ -1,48 +1,41 @@
 import React, { Component } from "react";
 import Habit from "./habit";
+import HabitAddForm from "./habitAddForm";
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Reading", count: 0 },
-      { id: 2, name: "Running", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
-  };
-
   handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const idx = habits.indexOf(habit);
-    habits[idx].count++;
-    this.setState({ habits });
+    this.props.onIncrement(habit);
   };
 
   handleDecreament = (habit) => {
-    const habits = [...this.state.habits];
-    const idx = habits.indexOf(habit);
-    const count = habits[idx].count - 1;
-    habits[idx].count = count < 0 ? 0 : count;
-    this.setState({ habits });
+    this.props.onDecreament(habit);
   };
 
   handleDelete = (habit) => {
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-    this.setState({ habits });
+    this.props.onDelete(habit);
+  };
+
+  handleAdd = (name) => {
+    this.props.onAdd(name);
   };
 
   render() {
     return (
-      <ul>
-        {this.state.habits.map((habit) => (
-          <Habit
-            key={habit.id}
-            habit={habit}
-            onIncrement={this.handleIncrement}
-            onDecreament={this.handleDecreament}
-            onDelete={this.handleDelete}
-          />
-        ))}
-      </ul>
+      <>
+        <HabitAddForm onAdd={this.handleAdd} />
+        <ul>
+          {this.props.habits.map((habit) => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.handleIncrement}
+              onDecreament={this.handleDecreament}
+              onDelete={this.handleDelete}
+            />
+          ))}
+        </ul>
+        <button>Reset All</button>
+      </>
     );
   }
 }
