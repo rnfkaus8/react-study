93ceda93886d21a9494edbe944d89f45b8d0e0d0
rnfkaus8 <kth4350@gmail.com>
2022-07-28 11:13:14 +0900

    modify : Navbar Component 반영, habits state handle method 반영

--- habbit-tracker/src/app.jsx
+++ habbit-tracker/src/app.jsx
@@ -1,9 +1,64 @@
 import "./app.css";
-import React from "react";
+import React, { Component } from "react";
 import Habits from "./components/habits";
+import Navbar from "./components/navbar";
 
-function App() {
-  return <Habits></Habits>;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const idx = habits.indexOf(habit);
+    habits[idx].count++;
+    this.setState({ habits });
+  };
+
+  handleDecreament = (habit) => {
+    const habits = [...this.state.habits];
+    const idx = habits.indexOf(habit);
+    const count = habits[idx].count - 1;
+    habits[idx].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  handleAdd = (name) => {
+    const habits = [
+      ...this.state.habits,
+      { id: Date.now(), name: name, count: 0 },
+    ];
+    this.setState({ habits });
+  };
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCount={
+            this.state.habits.filter((item) => {
+              return item.count > 0;
+            }).length
+          }
+        />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecreament={this.handleDecreament}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
