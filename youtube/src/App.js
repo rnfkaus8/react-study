f2ef8837275bab953a9a64637e1a8148fb0be3bd
rnfkaus8 <kth4350@gmail.com>
2022-08-01 18:18:35 +0900

    비디오 상세보기 component 추가, 화변 구성 변경

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -2,9 +2,11 @@ import React, {useEffect, useState} from 'react';
 import VideoList from "./components/video_list/VideoList";
 import VideoSearchForm from "./components/video_search/VideoSearchForm";
 import styles from "./App.module.css";
+import VideoDetail from "./components/video_detail/VideoDetail";
 
 const App = ({youtube}) => {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
   useEffect(() => {
     youtube
@@ -13,14 +15,33 @@ const App = ({youtube}) => {
   }, []);
 
   const handleSearch = (query) => {
+    setSelectedVideo(null);
     youtube
       .search(query)
       .then(videos => setVideos(videos));
   };
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  }
+
   return (
     <div className={styles.app}>
-      <VideoSearchForm onSearch={handleSearch} />
-      <VideoList videos={videos}/>
+      <VideoSearchForm onSearch={handleSearch}/>
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo}/>
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
+        </div>
+      </section>
     </div>
   );
 };
