1f07df3e050ca5d5e8bd201e97429b9b2abf4a57
rnfkaus8 <kth4350@gmail.com>
2022-12-11 19:12:40 +0900

    반응형 그리드 추가

--- next-study/nodebird/components/AppLayout.tsx
+++ next-study/nodebird/components/AppLayout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import {
+  Col, Input, Menu, Row,
+} from 'antd';
 
 const AppLayout = ({ children }: AppLayoutProps) => {
   return (
@@ -13,10 +15,23 @@ const AppLayout = ({ children }: AppLayoutProps) => {
           <Link href="/profile">profile</Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">signup</Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://www.zerocho.com" target="_blank" rel="noreferrer noopener">Made By ZeroCho</a>
+        </Col>
+      </Row>
     </div>
   );
 };
